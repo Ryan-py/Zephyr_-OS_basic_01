3b9134725bcc0eebc0774f9519a936fb9f15e706
Ryan-py <ryankiprotich42@gmail.com>
2023-08-11 00:08:32 +0300

    first commit

---
+++ build/image_preload.cmake
@@ -0,0 +1,9 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf9160dk_nrf9160_ns" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "/home/ryantech/Documents/nordic/code/ncs-fund/v2.x.x/lesson2/fund_less2_exer1/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CLI_CONFIG_DEBUG_OPTIMIZATIONS "y" CACHE INTERNAL "NCS child image controlled")
+set(CLI_CONFIG_DEBUG_THREAD_INFO "y" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "/home/ryantech/ncs/toolchains/1f9b40e71a/usr/local/bin/python3.8" CACHE INTERNAL "NCS child image controlled")
---
+++ build/zephyr/zephyr.dts.d
@@ -0,0 +1,24 @@
+empty_file.o: /home/ryantech/ncs/v2.4.1/zephyr/misc/empty_file.c \
+ /home/ryantech/ncs/v2.4.1/zephyr/boards/arm/nrf9160dk_nrf9160/nrf9160dk_nrf9160_ns.dts \
+ /home/ryantech/ncs/v2.4.1/zephyr/dts/arm/nordic/nrf9160ns_sica.dtsi \
+ /home/ryantech/ncs/v2.4.1/zephyr/dts/common/mem.h \
+ /home/ryantech/ncs/v2.4.1/zephyr/dts/arm/nordic/nrf91ns.dtsi \
+ /home/ryantech/ncs/v2.4.1/zephyr/dts/arm/armv8-m.dtsi \
+ /home/ryantech/ncs/v2.4.1/zephyr/dts/common/skeleton.dtsi \
+ /home/ryantech/ncs/v2.4.1/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ /home/ryantech/ncs/v2.4.1/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ /home/ryantech/ncs/v2.4.1/zephyr/include/zephyr/dt-bindings/adc/nrf-adc.h \
+ /home/ryantech/ncs/v2.4.1/zephyr/include/zephyr/dt-bindings/dt-util.h \
+ /home/ryantech/ncs/v2.4.1/zephyr/include/zephyr/sys/util_macro.h \
+ /home/ryantech/ncs/v2.4.1/zephyr/include/zephyr/sys/util_internal.h \
+ /home/ryantech/ncs/v2.4.1/zephyr/include/zephyr/sys/util_loops.h \
+ /home/ryantech/ncs/v2.4.1/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /home/ryantech/ncs/v2.4.1/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /home/ryantech/ncs/v2.4.1/zephyr/include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h \
+ /home/ryantech/ncs/v2.4.1/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ /home/ryantech/ncs/v2.4.1/zephyr/dts/common/freq.h \
+ /home/ryantech/ncs/v2.4.1/zephyr/dts/arm/nordic/override.dtsi \
+ /home/ryantech/ncs/v2.4.1/zephyr/dts/arm/nordic/nrf91_peripherals.dtsi \
+ /home/ryantech/ncs/v2.4.1/zephyr/boards/arm/nrf9160dk_nrf9160/nrf9160dk_nrf9160_common.dts \
+ /home/ryantech/ncs/v2.4.1/zephyr/boards/arm/nrf9160dk_nrf9160/nrf9160dk_nrf9160_common-pinctrl.dtsi \
+ /home/ryantech/ncs/v2.4.1/zephyr/boards/arm/nrf9160dk_nrf9160/nrf9160dk_nrf9160_partition_conf.dts
